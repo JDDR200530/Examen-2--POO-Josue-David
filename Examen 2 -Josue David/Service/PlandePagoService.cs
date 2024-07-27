1eefebf0dc4aa498c0aca1641d14d736dc30d13a
JDDR200530 <david1970josue@gmail.com>
2024-07-27 04:54:57 -0600

    Finalizando Examen

--- Examen 2 -Josue David/Service/PlandePagoService.cs
+++ Examen 2 -Josue David/Service/PlandePagoService.cs
@@ -1,9 +1,61 @@
-﻿using Examen_2__Josue_David.Service.Interface;
+﻿//using Examen_2__Josue_David.Entity;
+//using Examen_2__Josue_David.Service.Interface;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
 
-namespace Examen_2__Josue_David.Service
-{
-    public class PlandePagoService :IPlandePagoService
-    {
+//namespace Examen_2__Josue_David.Service
+//{
+//    public class PlandePagoService : IPlandePagoService
+//    {
+//        private readonly LoanContext _context; 
 
-    }
-}
+//        public PlandePagoService(LoanContext context)
+//        {
+//            _context = context;
+//        }
+
+//        public void CreateLoan(ClienteEntity cliente, PrestamoEntity prestamo)
+//        {
+//            _context.Clientes.Add(cliente); 
+//            _context.Prestamos.Add(prestamo);
+//            _context.SaveChanges();
+
+//            var cuotas = CalcularCuotas(prestamo);
+//            _context.Cuotas.AddRange(cuotas);
+//            _context.SaveChanges();
+//        }
+
+//        private List<PrestamoEntity> CalcularCuotas(PrestamoEntity prestamo)
+//        {
+//            var cuotas = new List<PrestamoEntity>();
+//            var fecha = prestamo.FechaPrimerPago;
+//            var monto = prestamo.Monto;
+//            var tasaInteres = prestamo.TasaInteres / 100 / 12;
+//            var plazo = prestamo.Plazo;
+
+//            var cuotaNivelada = monto * (decimal)(tasaInteres * Math.Pow(1 + (double)tasaInteres, plazo) / (Math.Pow(1 + (double)tasaInteres, plazo) - 1));
+
+//            for (int i = 0; i < plazo; i++)
+//            {
+//                var interes = monto * tasaInteres;
+//                var principal = cuotaNivelada - interes;
+//                monto -= principal;
+
+//                cuotas.Add(new Cuota
+//                {
+//                    NumeroCuota = i + 1,
+//                    FechaPago = fecha.AddMonths(i),
+//                    Dias = (i == 0) ? (fecha.AddMonths(i).Day - fecha.Day) : DateTime.DaysInMonth(fecha.AddMonths(i).Year, fecha.AddMonths(i).Month),
+//                    Interes = interes,
+//                    Principal = principal,
+//                    PagoNiveladoSinComision = cuotaNivelada,
+//                    PagoNiveladoConComision = cuotaNivelada * 1.01m, // Asumimos una comisión del 1%
+//                    SaldoPrincipal = monto
+//                });
+//            }
+
+//            return cuotas;
+//        }
+//    }
+//}
--- Examen 2 -Josue David/obj/Debug/net8.0/Examen 2 -Josue David.AssemblyInfo.cs
+++ Examen 2 -Josue David/obj/Debug/net8.0/Examen 2 -Josue David.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Examen 2 -Josue David")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bbe9f1a45f95585043a3516e2a38fd66ba3d4e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6ea86184ab630ea4e406323259691734743d5ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Examen 2 -Josue David")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Examen 2 -Josue David")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
